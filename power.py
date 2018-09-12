f956b29e2a1e34bc52892bf02ffaf3386e158c49
abishamathi <42571129+abishamathi@users.noreply.github.com>
2018-09-12 13:56:16 +0530

    Create power.py

---
+++ power.py
@@ -0,0 +1,8 @@
+x=2,3
+if(exp ==1):
+   return (2)
+if(exp !=1);
+   return (base*power(base,emp-1))
+base =int(input('enter the base:'))
+exp =int(input('enter exponential value:'))
+     print('result:',power(base,exp))
