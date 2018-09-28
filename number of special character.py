7139a8e2d4ee68b5246d4034563d9a8aad674939
abishamathi <42571129+abishamathi@users.noreply.github.com>
2018-09-28 17:41:39 +0530

    Create number of special character.py

---
+++ number of special character.py
@@ -0,0 +1,4 @@
+if(regex.search(string)==None):
+print('String is accepted')
+else:
+print('String is not accepted')https://github.com/abishamathi/python-program-/blob/master/number%20of%20numeric%20character.py
